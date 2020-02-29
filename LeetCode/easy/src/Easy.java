7944f6604757a00f48452a844b0d7e1e87d4c92f
Giggggggle <372112094@qq.com>
2020-02-29 23:16:53 +0800

    fifth commit

--- LeetCode/easy/src/Easy.java
+++ LeetCode/easy/src/Easy.java
@@ -111,6 +111,25 @@ public class Easy {
         }
     }
 
+    /*加1：给定一个由整数组成的非空数组所表示的非负整数，在该数的基础上加一。
+    * 最高位数字存放在数组的首位， 数组中每个元素只存储单个数字。
+    * 你可以假设除了整数 0 之外，这个整数不会以零开头。
+    * 输入：[1,2,3]  [4,3,2,1]  [9]
+    * 输出：[1,2,4]  [4,3,2,2]  [1,0]
+    * 算法：
+    * */
+    public static int[] plusOne(int[] digits) {
+        for (int i = digits.length - 1; i >= 0; i--) {
+            digits[i]++;
+            digits[i] = digits[i] % 10;
+            if (digits[i] != 0) return digits;
+        }
+        digits = new int[digits.length + 1];
+        digits[0] = 1;
+        return digits;
+    }
+
+
 }
 
 
